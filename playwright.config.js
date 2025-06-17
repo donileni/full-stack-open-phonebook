1b6b944a5fc7fc8ee82304340ed89659df8b5dea
David <david.markus.olsson@gmail.com>
2025-06-17 18:31:59 +0200

    Fixed configuration for playwright

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   webServer: {
-    command: "npm run start",
+    command: "npm run start:dev",
     url: "http://localhost:3001",
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
